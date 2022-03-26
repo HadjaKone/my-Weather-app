de0bc6d57b9c444842050b9105950de67befd13e
HadjaKone <hkone@smu.edu>
2022-03-26 13:21:32 -0500

    update time and display relevant icon

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-let weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  ,
-];
+function formartDate(timestamp) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    ,
+  ];
 
-let now = new Date();
-let hour = now.toLocaleTimeString();
-let day = weekdays[now.getDay()];
-let currentDate = document.querySelector("#currentDate");
-currentDate.innerHTML = `${day} ${hour}`;
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = weekdays[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
 function showCityWeather(response) {
-  console.log(response.data.main);
+  console.log(response.data);
+
+  celsiusTemperature = response.data.main.temp;
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  let date = document.querySelector("#currentDate");
+  date.innerHTML = formartDate(response.data.dt * 1000);
 
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#changeTemp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#changeTemp").innerHTML =
+    Math.round(celsiusTemperature);
   document.querySelector(
     "#humidity"
   ).innerHTML = `humidity:${response.data.main.humidity}%`;
@@ -66,7 +83,29 @@ function getPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(currentTemperature);
 }
-navigator.geolocation.getCurrentPosition(currentTemperature);
-let currentSearch = document.querySelector("#current-location-button");
+// navigator.geolocation.getCurrentPosition(currentTemperature);
+// let currentSearch = document.querySelector("#current-location-button");
 
-currentSearch.addEventListener("click", getPosition);
+// currentSearch.addEventListener("click", getPosition);
+function FahrenheitToCelsus(event) {
+  event.preventDefault();
+  //remove the active class the celsius link
+  fahTemp.classList.remove("active");
+  celsusTemp.classList.add("active");
+  document.querySelector("#changeTemp").innerHTML =
+    Math.round(celsiusTemperature);
+}
+function celsusToFahrenheit(event) {
+  event.preventDefault();
+  celsusTemp.classList.remove("active");
+  fahTemp.classList.add("active");
+  let temperatureElement = document.querySelector("#changeTemp");
+  let ftemp = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(ftemp);
+}
+let celsiusTemperature = null;
+let fahTemp = document.querySelector("#fahrenheit-link");
+fahTemp.addEventListener("click", celsusToFahrenheit);
+
+let celsusTemp = document.querySelector("#celsus-link");
+celsusTemp.addEventListener("click", FahrenheitToCelsus);
